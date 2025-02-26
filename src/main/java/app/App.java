7903ee921187e983bc62b874c5b77bfdc20ce378
T1r4m1su <117399429+T1r4m1su@users.noreply.github.com>
2025-02-26 10:54:36 +0100

    what what

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -1,15 +1,12 @@
 package app;
+import java.util.Scanner;
+
 import app.util.WsClient;
 
 public class App {
+
 	public static void main(String[] args) {
 		WsClient c = new WsClient("http://localhost/scientology_market/smapi.php");
-
-		try {
-			System.out.println(c.getPopolaritaSedi());
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		c.menuScelta();
 	}
 }
--- src/main/java/app/util/WsClient.java
+++ src/main/java/app/util/WsClient.java
@@ -5,6 +5,7 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.Scanner;
 
 import app.errors.WsException;
 import app.model.Clienti;
@@ -72,5 +73,92 @@ public class WsClient {
 		// System.out.println("received: " + body);
 		return XmlUtils.unmarshal(PopolaritaSedi.class, body);
 	}
+
+		
+
+	public void menuScelta(){
+
+		Scanner in = new Scanner(System.in);
+		boolean nExit = true; 
+
+		while(nExit){
+			System.out.println(
+				"1) Per inserire una nuova tessera insieme a cliente \n"+
+				"2) Per stampare le tessere \n"+
+				"3) Per stampare le sedi \n"+
+				"4) Per stampare i clienti registrati \n"+
+				"5) Per Uscire \n"
+			);
+			int scelta= in.nextInt();
+			switch (scelta) {
+				case 1:
+					
+					break;
+				case 2:
+					System.out.println(
+						"1) Per stampare tutte le tessere \n"+
+						"2) Per cercare le tessere in base al Nome e al Cognome \n"
+					);
+					scelta= in.nextInt();
+					switch (scelta) {
+						case 1:
+							try {
+								System.out.println(getTessere());
+							} catch (Exception e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+							break;
+						case 2:
+							
+							break;
+
+						default:
+							break;
+					}
+					break;
+				case 3:
+					System.out.println(
+						"1) Per stampare tutte le sedi \n"+
+						"2) Per cercare una sede in base al nome e indirizzo \n"+
+						"3) Per stampare il ragguaglio della creazione delle tessere \n"
+					);
+					scelta= in.nextInt();
+					switch (scelta) {
+						case 1:
+							
+							break;
+						case 2:
+							
+							break;
+						case 3:
+							
+							break;
+						default:
+							break;
+					}
+					break;
+				case 4:
+					scelta= in.nextInt();
+					switch (scelta) {
+						case 1:
+							
+							break;
+					
+						default:
+							break;
+					}
+					break;
+				case 5:
+					System.out.println("stai pere uscire dal programma");
+					nExit = false; 
+					in.close();
+					break;
+				default:
+					break;
+			}
+		}
+	}
+
 
 }
